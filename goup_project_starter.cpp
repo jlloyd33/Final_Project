8617babac637eca3938f55e965e43a2515c8370d
jlloyd33 <j.lloyd9218@student.nu.edu>
2025-08-24 10:21:52 -0600

    Add files via upload

---
+++ goup_project_starter.cpp
@@ -0,0 +1,18 @@
+/* ---------------------------------------------------------------------------
+** I attest that this following code represents my own work and is subject to
+** the plagiarism policy found in the course syllabus.
+**
+** Class: 	CSC 242
+** Assignment: 	Lesson #
+** File: 	source.cpp
+** Description:
+**
+** Author: 	
+** Date: 	
+** -------------------------------------------------------------------------*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
