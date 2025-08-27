3b6fae677e05f80218ecd06ec1c8c305103d2173
Joshua Najera <j.najera9864@student.nu.edu>
2025-08-27 13:25:34 -0600

    Update goup_project_starter.cpp
    Here is what I was able to come up with for the first part of the first section of the project.

--- goup_project_starter.cpp
+++ goup_project_starter.cpp
@@ -13,10 +13,35 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 int main()
 {
+    
+	ifstream infile; //initialize the file
+	infile.open("words.txt"); // have to include the filepath to the file in the parentheses.
+
+	vector<string> library;
+	string words;
+
+	//Verify that the file actually opened.
+    //If the file opens correctly, the while loop populates the "library vector.
+    //If it was unsuccessful, the else statement will output an error message
+	if (infile.is_open())
+	{
+		while (infile >> words) // This loop populates the library vector.
+		{
+			library.push_back(words);
+		}
+		infile.close();
+	}
+	else
+	{
+		cout << "Error: words.txt could not be opened." << endl;
+	}
+	cout << "Number of words read: " << library.size() << endl; // This tells you the size of the library vector.
 
     return 0;
 }
